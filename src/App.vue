84173672b70ca6396026f2989ef298c5cb78df39
will.niu <will.niu@nio.com>
2023-01-30 15:30:01 +0800

    feat: add test code

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue';
 
 <template>
   <h>
-    Hello Will niu999
+    Hello Will niu999ssss
   </h>
   <div>
     <a href="https://vitejs.dev" target="_blank">
